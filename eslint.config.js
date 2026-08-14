90fc5c7bfdd578707a9adea161f6ffd2de98fd6e
Builder106 <vaughanolayinka@gmail.com>
2026-08-14 10:56:33 -0700

    chore: add ESLint and Prettier configuration for code quality

---
+++ eslint.config.js
@@ -0,0 +1,42 @@
+import js from '@eslint/js'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import tseslint from 'typescript-eslint'
+import prettier from 'eslint-config-prettier'
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  react.configs.recommended,
+  reactHooks.configs.recommended,
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true },
+      },
+      globals: {
+        browser: true,
+        es2024: true,
+        node: true,
+      },
+    },
+    settings: {
+      react: { version: '18.3' },
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+    },
+  },
+  prettier,
+  {
+    ignores: ['dist/', 'node_modules/', '*.config.*', 'scripts/', 'e2e/', 'server/'],
+  }
+)
---
+++ server/eslint.config.js
@@ -0,0 +1,31 @@
+import js from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import prettier from 'eslint-config-prettier'
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+      globals: {
+        node: true,
+        es2024: true,
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+    },
+  },
+  prettier,
+  {
+    ignores: ['dist/', 'node_modules/', '*.config.*'],
+  }
+)
